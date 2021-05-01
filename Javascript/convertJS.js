36799c4cb7bf1583548d80c5cbafe594f19b0962
Rabeya Khatun Muna <54771575+RabeyaMuna@users.noreply.github.com>
2021-05-02 05:19:52 +0600

    Update convertJS.js

--- Javascript/convertJS.js
+++ Javascript/convertJS.js
@@ -1,30 +1,44 @@
-const currencyOne = document.getElementById('currency-one');
-const currencyTwo = document.getElementById('currency-two');
-const amountone = document.getElementById('amount-one');
-const amountwo = document.getElementById('amount-two');
-const rate = document.getElementById('rate');
-const swap = document.getElementById('swap');
+var gbp, usd, eur, cad, aud;
 
-function calculate(){
-    const cuurone = currencyOne.value;
-    const currtwo = currencyTwo.value;
-    fetch(`https://api.exchangeratesapi.io/latest?base=${cuurone}`)
-    .then(res=>res.json())
-    .then(data=>{
-        const rateE = data.rates[currencyTwo.value];
-        rate.innerText = `1 ${cuurone}=${rateE} ${currtwo}`;
-        // amountwo.value = (amountone.value * rateE).toFixed(2);
-    })
+function init() {
+    gbp = document.getElementById("GBP");
+    usd = document.getElementById("USD");
+    eur = document.getElementById("EUR");
+    cad = document.getElementById("CAD");
+    aud = document.getElementById("AUD");
 }
-currencyOne.addEventListener('change',calculate);
-currencyTwo.addEventListener('change', calculate);
-amountone.addEventListener('input', calculate);
-amountwo.addEventListener('input', calculate);
-swap.addEventListener('click',()=>{
-    const temp = currencyOne.value;
-    currencyOne.value = currencyTwo.value;
-    currencyTwo.value=temp;
-    calculate();
+
+function gbpfunc() {
+    usd.value = parseFloat(gbp.value) * 0.49246;
+    eur.value = parseFloat(gbp.value) * 0.69714;
+    cad.value = parseFloat(gbp.value) * 0.50221;
+    aud.value = parseFloat(gbp.value) * 0.43497;
 }
-)
-calculate()
+
+function eurfunc() {
+    gbp.value = parseFloat(eur.value) * 1.43448;
+    usd.value = parseFloat(eur.value) * 0.70641;
+    cad.value = parseFloat(eur.value) * 0.72037;
+    aud.value = parseFloat(eur.value) * 0.62382;
+}
+
+function cadfunc() {
+    gbp.value = parseFloat(cad.value) * 1.99169;
+    usd.value = parseFloat(cad.value) * 0.98054;
+    eur.value = parseFloat(cad.value) * 1.38814;
+    aud.value = parseFloat(cad.value) * 0.86613;
+}
+
+function audfunc() {
+    gbp.value = parseFloat(aud.value) * 2.29964;
+    usd.value = parseFloat(aud.value) * 1.13262;
+    eur.value = parseFloat(aud.value) * 1.60329;
+    cad.value = parseFloat(aud.value) * 0.88297;
+}
+
+function usdfunc() {
+    gbp.value = parseFloat(usd.value) * 2.03032;
+    eur.value = parseFloat(usd.value) * 1.41544;
+    cad.value = parseFloat(usd.value) * 1.01941;
+    aud.value = parseFloat(usd.value) * 0.88297;
+}
